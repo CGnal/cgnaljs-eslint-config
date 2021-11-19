0afdfdad3d17a395bc4e549c7da8471f7c09f847
Andrea Scartabelli <andrea.scartabelli@gmail.com>
2021-11-19 11:52:09 +0100

    [closes #6] Add missing jsx-quotes rule

--- react/base.js
+++ react/base.js
@@ -1,5 +1,6 @@
 module.exports = {
     rules: {
+        "jsx-quotes": ["error", "prefer-double"],
         "react/jsx-boolean-value": [2, "always"],
         "react/jsx-indent-props": ["error", 4]
     }
